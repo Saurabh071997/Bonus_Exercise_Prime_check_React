45e2efda48a5b64fe36f1f68fa12183177d8dfc1
Saurabh071997 <37976922+Saurabh071997@users.noreply.github.com>
2020-12-27 20:50:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,64 +2,90 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [item, setItem] = useState(0);
+  const [item, setItem] = useState("");
   const [result, setResult] = useState("");
 
   function changeHandler(event) {
-    // console.log(event.target.value);
     setItem(event.target.value);
   }
 
   function clickHandler() {
-    //  console.log("hjhjhj");
-    console.log(item);
+    // console.log(typeof item);
 
-    if (item == 1) {
-      console.log("one");
-      setResult("1 is neither a prime nor a composite");
+    if (item.length === 0) {
+      setResult("Enter a valid number");
       return;
     }
 
-    for (var i = 2; i <= item / 2; i++) {
-      if (item % i === 0) {
-        setResult(item + " is not Prime Number");
+    var val = parseInt(item, 10);
+    // console.log(typeof val);
+
+    if (val === 1 || val === 0) {
+      setResult(" NON PRIME ");
+      return;
+    }
+
+    for (var i = 2; i <= val / 2; i++) {
+      if (val % i === 0) {
+        setResult(" NON PRIME ");
         return;
       }
     }
 
-    setResult(item + " is a Prime Number");
+    setResult(" PRIME ");
   }
 
   return (
     <div className="App">
       <div className="header">
         <div className="navigation">
-          <h1> Prime Check </h1>
+          <h1 style={{ fontSize: "4rem" }}> Prime Check </h1>
         </div>
       </div>
 
-      <div className="conatiner" style={{ maxWidth: "600px", margin: "auto" }}>
-        <input
-          id="txt-input"
-          onChange={changeHandler}
-          style={{
-            width: "50%",
-            padding: "1rem",
-            display: "block",
-            margin: "auto"
-          }}
-        ></input>
+      <div className="body-container">
+        <div className="container">
+          <input
+            style={{
+              width: "60%",
+              padding: "0.5rem",
+              fontSize: "1.5rem",
+              borderRadius: "0.5rem",
+              margin: "0rem auto",
+              background: "transparent",
+              color: "white",
+              fontWeight: "bold"
+            }}
+            placeholder="Enter number"
+            onChange={changeHandler}
+          ></input>
 
-        <button
-          // onClick={(event) => clickHandler(event)}
-          onClick={clickHandler}
-          style={{ margin: "1rem auto" }}
-        >
-          {" "}
-          Check
-        </button>
-        {/* {result} */}
-        <div>{result}</div>
+          <button
+            style={{
+              width: "40%",
+              padding: "0.25rem",
+              margin: "1rem auto",
+              borderRadius: "0.5rem",
+              color: "white",
+              background: "transparent",
+              fontWeight: "bold",
+              fontSize: "1rem"
+            }}
+            onClick={clickHandler}
+          >
+            CHECK PRIME
+          </button>
+
+          <div
+            style={{
+              fontSize: "2rem",
+              margin: "0.5rem auto 1.25rem auto",
+              fontWeight: "bold"
+            }}
+          >
+            {result}
+          </div>
+        </div>
       </div>
     </div>
   );
